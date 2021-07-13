81b0f81e798079a2306240cb8260e2619d13c9b7
IsaacChiang <isaacchiang@kkstream.com>
2021-07-13 11:25:18 +0800

    support menu item

--- sample/PlaycraftDemo/app/src/main/java/com/kkstream/playcraftdemo/PlaybackActivity.kt
+++ sample/PlaycraftDemo/app/src/main/java/com/kkstream/playcraftdemo/PlaybackActivity.kt
@@ -3,11 +3,15 @@ package com.kkstream.playcraftdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
 import androidx.activity.viewModels
+import com.kkstream.android.paas.player.common.PaaSController
 import com.kkstream.android.paas.player.common.PaaSProvider
 import com.kkstream.android.paas.player.common.callback.DialogEventListener
 import com.kkstream.android.paas.player.common.event.error.PaaSErrorEvent
 import com.kkstream.android.paas.player.mobile.DefaultDialogEventListener
+import com.kkstream.android.paas.player.mobile.DefaultMenuFactory
 import com.kkstream.playcraftdemo.databinding.ActivityPlaybackBinding
 import com.kkstream.playcraftdemo.viewmodel.PlaybackViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,9 +21,13 @@ class PlaybackActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPlaybackBinding
     private val viewModel: PlaybackViewModel by viewModels()
 
+    private val paaSController: PaaSController?
+        get() = paasProvider?.getController()
     private var paasProvider: PaaSProvider? = null
     private val dialogEventListener: DialogEventListener
         get() = DefaultDialogEventListener(this, binding.kksPlayerServiceView)
+    private val menuFactory: DefaultMenuFactory
+        get() = DefaultMenuFactory(binding.kksPlayerServiceView)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,6 +47,35 @@ class PlaybackActivity : AppCompatActivity() {
         }
 
         setupDialogListener()
+
+        binding.kksPlayerServiceView.setupMenuItems(getMenuItem())
+    }
+
+    /**
+     * add customize and player's default view in the right-top corner of PaaSView
+     */
+    private fun getMenuItem(): List<View> {
+        return listOf(
+            menuFactory.createCastMenuItem(),
+            menuFactory.createSettingMenuItem(),
+            createSampleMenu()
+        )
+    }
+
+    /**
+     * Customize view
+     */
+    private fun createSampleMenu(): View {
+        val inflater = LayoutInflater.from(this)
+        val menu =
+            inflater.inflate(R.layout.widget_playback_menu_sample, binding.kksPlayerServiceView, false)
+        menu.setOnClickListener {
+            paaSController?.pause()
+
+            // TODO do something
+        }
+
+        return menu
     }
 
     /**
